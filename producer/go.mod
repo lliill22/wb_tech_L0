739571cf0d490b42ab5880f13950a06fb84c455f
Bogdan Osotov <osotovb@gmail.com>
2025-09-05 18:12:03 +0300

    added video

--- producer/go.mod
+++
@@ -1,5 +0,0 @@
-module producer
-
-go 1.24.5
-
-require github.com/confluentinc/confluent-kafka-go/v2 v2.11.0
--- producer/topic-creation.sh
+++
@@ -1,7 +0,0 @@
-docker run --rm --network=host confluentinc/cp-kafka:7.5.0 \
-  kafka-topics \
-  --create \
-  --topic my-topic \
-  --bootstrap-server localhost:29092 \
-  --partitions 1 \
-  --replication-factor 1
